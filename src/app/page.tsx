f172cc25b55638c25de5ebaf0d27722a06f14e5b
RobelRH <robel.redda28@gmail.com>
2023-09-24 19:39:30 +0300

    initial layout ready

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import Content from '@/components/content'
+import Hero from '@/components/hero'
+import Team from '@/components/team'
+import Testimonial from '@/components/testimonial'
+
+export default function Home() {
+  return (
+    <main>
+      <Hero />
+      <Content />
+      <Team />
+      <Testimonial />
+    </main>
+  )
+}
